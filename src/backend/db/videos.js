40c867c448ce624164b4e0a5bb14092e403600af
Maitra Khatri <maitrakhatri@gmail.com>
2022-05-12 13:29:09 +0530

    basic setup, database and assets

---
+++ src/backend/db/videos.js
@@ -0,0 +1,151 @@
+/**
+ * Videos Database can be added here.
+ * You can add videos of your wish with different attributes
+ * */
+
+ export const videos = [
+  {
+    _id: "U-BM-FZH0Ik",
+    title: "Warner Brothers will Delete Superman and Batman 😨 || Men of culture 6",
+    channelName: "Men of culture",
+    link: function () {
+      return `https://www.youtube.com/watch?v=${this._id}`;
+    },
+    thumb: function () {
+      return `https://i.ytimg.com/vi/${this._id}/hq720.jpg`
+    },
+    categoryName: "Podcast",
+  },
+  {
+    _id: "Al1jCxSzxcY",
+    title: "Moon Knight Episode 1 Breakdown in Hindi | DesiNerd",
+    channelName: "DesiNerd",
+    link: function () {
+      return `https://www.youtube.com/watch?v=${this._id}`;
+    },
+    thumb: function () {
+      return `https://i.ytimg.com/vi/${this._id}/hq720.jpg`
+    },
+    categoryName: "Breakdown",
+  },
+  {
+    _id: "e0ykpmZlRwY",
+    title: "Thor: Goddess of Thunder | Episode 01 | Marvel Comics in Hindi",
+    channelName: "SuperSuper Comics",
+    link: function () {
+      return `https://www.youtube.com/watch?v=${this._id}`;
+    },
+    thumb: function () {
+      return `https://i.ytimg.com/vi/${this._id}/hq720.jpg`
+    },
+    categoryName: "Comics",
+  },
+  {
+    _id: "NUV8Xfu4bUE",
+    title: "When You're WRONG and Villains are RIGHT - PJ Explained",
+    channelName: "PJ Explained",
+    link: function () {
+      return `https://www.youtube.com/watch?v=${this._id}`;
+    },
+    thumb: function () {
+      return `https://i.ytimg.com/vi/${this._id}/hq720.jpg`
+    },
+    categoryName: "Explained",
+  },
+  {
+    _id: "ktKwHLqW5hg",
+    title: "The Batman Movie Review & Post Credit Scene | Super Reviews",
+    channelName: "SuperSuper",
+    link: function () {
+      return `https://www.youtube.com/watch?v=${this._id}`;
+    },
+    thumb: function () {
+      return `https://i.ytimg.com/vi/${this._id}/hq720.jpg`
+    },
+    categoryName: "Breakdown",
+  },
+  {
+    _id: "XNFMw9Fz_Bc",
+    title: "the LOKI Obsession: Why He is Better than any Other MCU Character? - PJ Explained",
+    channelName: "PJ Explained",
+    link: function () {
+      return `https://www.youtube.com/watch?v=${this._id}`;
+    },
+    thumb: function () {
+      return `https://i.ytimg.com/vi/${this._id}/hq720.jpg`
+    },
+    categoryName: "Explained",
+  },
+  {
+    _id: "HUu7tafIIbk",
+    title: "Why THE BOYS is Best Superhero Series ? | Nerd Talks Ep 30",
+    channelName: "DesiNerd",
+    link: function () {
+      return `https://www.youtube.com/watch?v=${this._id}`;
+    },
+    thumb: function () {
+      return `https://i.ytimg.com/vi/${this._id}/hq720.jpg`
+    },
+    categoryName: "Explained",
+  },
+  {
+    _id: "gRvkh4LwuwE",
+    title: "Kabhi Doodh me Thumbs-Up mila ke piya hai? 🤮☠ || Men of Culture 8",
+    channelName: "Men of Culture",
+    link: function () {
+      return `https://www.youtube.com/watch?v=${this._id}`;
+    },
+    thumb: function () {
+      return `https://i.ytimg.com/vi/${this._id}/hq720.jpg`
+    },
+    categoryName: "Podcast",
+  },
+  {
+    _id: "h4xma_jUglM",
+    title: "Every Character You Will See in The Batman | SuperSuper",
+    channelName: "SuperSuper",
+    link: function () {
+      return `https://www.youtube.com/watch?v=${this._id}`;
+    },
+    thumb: function () {
+      return `https://i.ytimg.com/vi/${this._id}/hq720.jpg`
+    },
+    categoryName: "Breakdown",
+  },
+  {
+    _id: "uMwAu51zqAc",
+    title: "aaj se humko Marvel aur DC ki jarurat nahi 😜🙈 || Men of Culture 10",
+    channelName: "Men of Culture",
+    link: function () {
+      return `https://www.youtube.com/watch?v=${this._id}`;
+    },
+    thumb: function () {
+      return `https://i.ytimg.com/vi/${this._id}/hq720.jpg`
+    },
+    categoryName: "Podcast",
+  },
+  {
+    _id: "T_E1WpAImxw",
+    title: "DEADPOOL Kills the Marvel Universe | Episode 01 | Marvel Comics in Hindi",
+    channelName: "SuperSuper",
+    link: function () {
+      return `https://www.youtube.com/watch?v=${this._id}`;
+    },
+    thumb: function () {
+      return `https://i.ytimg.com/vi/${this._id}/hq720.jpg`
+    },
+    categoryName: "Comics",
+  },
+  {
+    _id: "YseedL-ZkkE",
+    title: "First Indian Anti-SuperHero: Aparichit (ReThink) - PJ Explained",
+    channelName: "PJ Explained",
+    link: function () {
+      return `https://www.youtube.com/watch?v=${this._id}`;
+    },
+    thumb: function () {
+      return `https://i.ytimg.com/vi/${this._id}/hq720.jpg`
+    },
+    categoryName: "Explained",
+  },
+];
